eea1df37bcc07ed857ff040750d971111578c230
Ahmed Bizo <ahmedmajedb@hotmail.com>
2020-10-13 00:08:22 +0200

    i am rich

---
+++ I AM RICH/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by AHMED BIZO on 2019-12-23.
+//  Copyright © 2019 AHMED BIZO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
